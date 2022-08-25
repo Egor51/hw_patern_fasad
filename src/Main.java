0fcd4c00135a6553e4dee63d353020ef14488c0e
Egor51 <kubrakov_egor87@mail.ru>
2022-08-25 12:07:04 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("1101100111","10101"));
+        System.out.println(bins.mult("01100111","0110"));
+    }
+}
